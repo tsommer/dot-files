c4ddda3f7fef9b22440eb52884515c78bb7cbb57
Tom <tom@trikeapps.com>
2011-03-28 21:28:36 +1100

    Fix clone and link script.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -3,7 +3,9 @@
 cd &&
 [ -d '.dot-files' ] || git clone git://github.com/tsommer/dot-files.git .dot-files &&
 find .dot-files/files -depth 1 | while read f; do
-  #[ "$f" == '.dot-files/files/.git' ] ||
-  #([ "$f" == '.dot-files/files/oh-my-zsh' ] && ln -vsf "$f" .oh-my-zsh) ||
+  ([ "$f" == '.dot-files/files/oh-my-zsh' ]) ||
   ln -vsf "$f" .
+done &&
+find .dot-files/files/oh-my-zsh -depth 1 | while read f; do
+  ln -vsf ~/"$f" .oh-my-zsh/custom
 done
