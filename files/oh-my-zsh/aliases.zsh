513f4dd74483b421d0bd91bfbe9d7513db5a2937
Tom <tom@trikeapps.com>
2011-04-19 16:55:49 +1000

    Add aliases for easy project access.

--- files/oh-my-zsh/aliases.zsh
+++ files/oh-my-zsh/aliases.zsh
@@ -4,3 +4,17 @@ alias gm='git merge'
 alias ga='git add'
 
 alias rgm='ruby script/rails generate migration'
+
+alias vup='vagrant up'
+alias vdown='vagrant halt'
+alias vssh='vagrant ssh'
+
+function goto_project_and_up() {
+  cd ~/dev/$1 && vup
+}
+alias pup='goto_project_and_up'
+
+function goto_project_and_ssh() {
+  cd ~/dev/$1 && vssh
+}
+alias pssh='goto_project_and_ssh'
