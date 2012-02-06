80aaf064f5c12c395cc71fab52aec6d79630c593
Tom <tom@trikeapps.com>
2012-02-06 20:13:43 +1100

    Fix clone and link script for linux.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -2,15 +2,14 @@
 
 cd &&
 [ -d '.dot-files' ] || git clone git://github.com/tsommer/dot-files.git .dot-files &&
-find .dot-files/files -depth 1 | while read f; do
+find .dot-files/files -maxdepth 1 | while read f; do
   ([ "$f" == '.dot-files/files/oh-my-zsh' ]) ||
 	([ "$f" == '.dot-files/files/bin' ]) ||
   ln -vsf "$f" .
 done &&
-find .dot-files/files/oh-my-zsh -depth 1 | while read f; do
+find .dot-files/files/oh-my-zsh/* -maxdepth 1 | while read f; do
   ln -vsf ~/"$f" .oh-my-zsh/custom
 done
-done
-find .dot-files/files/bin -depth 1 | while read f; do
+find .dot-files/files/bin/* -maxdepth 1 | while read f; do
   ln -vsf ~/"$f" /usr/local/bin
 done
