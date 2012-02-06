dacb55985cf625aa8917774d5c395553ab5003f6
Tom <tom@trikeapps.com>
2012-02-06 20:06:35 +1100

    Remove sublime from dot files.

--- clone_and_link.sh
+++ clone_and_link.sh
@@ -4,15 +4,12 @@ cd &&
 [ -d '.dot-files' ] || git clone git://github.com/tsommer/dot-files.git .dot-files &&
 find .dot-files/files -depth 1 | while read f; do
   ([ "$f" == '.dot-files/files/oh-my-zsh' ]) ||
-  ([ "$f" == '.dot-files/files/sublime' ]) ||
 	([ "$f" == '.dot-files/files/bin' ]) ||
   ln -vsf "$f" .
 done &&
 find .dot-files/files/oh-my-zsh -depth 1 | while read f; do
   ln -vsf ~/"$f" .oh-my-zsh/custom
 done
-find .dot-files/files/sublime -depth 1 | while read f; do
-  ln -vsf ~/"$f" ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 done
 find .dot-files/files/bin -depth 1 | while read f; do
   ln -vsf ~/"$f" /usr/local/bin
