38c36a4c7de6664cedc0780ff4f403b091eb8de6
Tom <bballts14@gmx.de>
2012-08-14 16:01:53 +1000

    Add a few more aliases.

--- files/oh-my-zsh/aliases.zsh
+++ files/oh-my-zsh/aliases.zsh
@@ -7,10 +7,18 @@ alias gcf='git clean -f'
 
 alias rgm='ruby script/rails generate migration'
 
+alias rsp='rake spec'
+alias rcu='rake cucumber'
+alias rfe='rake features'
+
 alias v='vagrant'
 alias vup='vagrant up'
 alias vdown='vagrant halt'
 alias vssh='/usr/local/bin/colored-vssh'
+
+alias m.='mvim .'
+
+alias a='ack'
 
 function goto_project() {
   cd ~/dev/$1
