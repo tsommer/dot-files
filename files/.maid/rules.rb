272748ea039123b54df488d62ad24718a36f7fba
Tom <bballts14@gmx.de>
2013-03-17 20:29:35 +1100

    Update maid rules to remove deprecation warnings.

--- files/.maid/rules.rb
+++ files/.maid/rules.rb
@@ -2,12 +2,12 @@ Maid.rules do
   rule "Old files downloaded" do
     dir("~/Downloads/*").each do |path|
       if File.file?(path)
-        trash(path) if 1.week.since?(last_accessed(path))
+        trash(path) if 1.week.since?(accessed_at(path))
       else
         glob_path = path.gsub(/\[.+\]/, "*")
         files     = Dir[File.join(glob_path, "**/*.*")]
 
-        trash(path) if files.all? { |file| 1.week.since?(last_accessed(file)) }
+        trash(path) if files.all? { |file| 1.week.since?(accessed_at(file)) }
       end
     end
   end
