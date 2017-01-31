65bbe6e634cf9c0cb903ec9afc3ceedf624daa72
Tom Sommer <tomsommer@Toms-MacBook-Pro-2.local>
2017-01-31 13:39:58 +1100

    Remove most of the fancyness

--- update_and_link.sh
+++ update_and_link.sh
@@ -9,12 +9,8 @@ cd &&
 find .dot-files/files -maxdepth 1 | while read f; do
   ([ "$f" == '.dot-files/files' ]) ||
   ([ "$f" == '.dot-files/files/oh-my-zsh' ]) ||
-  ([ "$f" == '.dot-files/files/bin' ]) ||
   ln -vsf "$f" .
 done &&
 find .dot-files/files/oh-my-zsh/* -maxdepth 1 | while read f; do
   ln -vsf ~/"$f" .oh-my-zsh/custom
-done
-find .dot-files/files/bin/* -maxdepth 1 | while read f; do
-  ln -vsf ~/"$f" /usr/local/bin
 done
